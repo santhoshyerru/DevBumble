218a62f87793e2c680c6ddce894c8e663aa17dbe
santhoshsis <santhosh.y@sourceitsolutions.com>
2025-11-19 00:04:59 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Hello Man!");
+});
+app.use("/hello", (req, res) => {
+  res.send("Hello from the server!");
+});
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});

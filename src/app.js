0b4b0937c851e86dcdd5674e019060a8f01d2ed8
santhoshsis <santhosh.y@sourceitsolutions.com>
2025-11-19 00:34:39 -0500

    Added get/post/put/delete/patch routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello Man!");
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John",
+    age: 20,
+    city: "New York",
+  });
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello from the server!");
+
+app.post("/user", (req, res) => {
+  res.send("User created successfully");
 });
+
+app.put("/user", (req, res) => {
+  res.send("User updated successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User patched successfully");
+});
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello from the server!");
+// });
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
